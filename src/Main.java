2a0ee5070086d95d8e3f724bf271a4780a80484a
olga.prokopova <lookluckalisa@gmail.com>
2020-11-29 15:08:41 +0300

    added new objects in class Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        Calculator calculator1 = new Calculator(21, "add", 2.3);
+        Calculator calculator2 = new Calculator(21, "substract", 2.3);
+        Calculator calculator3 = new Calculator(21, "multiplication", 2.3);
+        Calculator calculator4 = new Calculator(21, "division", 2.3);
+    }
 }
